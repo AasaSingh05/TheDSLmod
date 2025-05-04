7ad736fed5d837d4dc257031abd1173172db3a91
AasaSingh05 <aasasingh2005@gmail.com>
2025-05-05 01:18:16 +0530

    Feat: added all the basic framework needed for the item additions

--- src/main/java/net/bige/tutorialmod/TutorialModClient.java
+++ src/main/java/net/bige/tutorialmod/TutorialModClient.java
@@ -1,11 +1,12 @@
 package net.bige.tutorialmod;
 
+import net.bige.tutorialmod.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TutorialModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
+        ModItems.registerModItems();
     }
 }
---
+++ src/main/java/net/bige/tutorialmod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.bige.tutorialmod.item;
+
+import net.bige.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    //actual item registration here
+
+
+    //this is to register the item into the game files
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    //this is to register that there are items in the dsl modpack
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering items for mod: " + TutorialMod.MOD_ID);
+
+//        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+//            fabricItemGroupEntries.add();
+//        });
+    }
+}
